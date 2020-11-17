2a961bef8d631c4643f70684692591166e475b76
lycGitTemp <2745017672@qq.com>
2020-11-17 08:41:31 +0800

    第十四次课

---
+++ 第十四次课/3.js
@@ -0,0 +1,8 @@
+function orange(){
+    var array=[123,23,34,45,68,59,1,23,38,41]
+    for(let index=0;index<array.length;index++){
+        const element=array[index];
+        console.log(element);
+    }
+}
+orange();
